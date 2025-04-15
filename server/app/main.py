19a5de104b93fbe6d87f33328af0f75cdec4693d
Leonardo Seminatore <l02.seminatore@gmail.com>
2025-04-15 11:49:55 +0200

    add db connection

--- server/app/main.py
+++ server/app/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import shutil
 import uuid
 import zipfile
+from crud import insert_waypoint
 
 app = FastAPI()
 
@@ -28,6 +29,8 @@ async def upload_zip(file: UploadFile = File(...)):
             os.makedirs(extract_dir, exist_ok=True)
             # unzip file
             zip_ref.extractall(extract_dir)
+            
+        logger.info(f"Unpacked zip file.")    
         
         # open trip and extract data (need to us for loop later)
         with open(f"{extract_dir}/trip/marokko_14732894/trip.json", "r", encoding="utf-8") as f:
@@ -40,8 +43,9 @@ async def upload_zip(file: UploadFile = File(...)):
 
         # select relevant columns
         auswahl = df[["id", "display_name", "description", "weather_condition", "weather_temperature", "uuid", "location_lon", "location_lat"]]
-        
-        logger.info(f"Ausgewählte Personen: {auswahl}")
+                
+        for index, row in auswahl.iterrows():
+            insert_waypoint(row, logger)
         
         # delete created directory
         shutil.rmtree(extract_dir)
---
+++ server/crud.py
@@ -0,0 +1,21 @@
+from database import get_db_connection
+
+def insert_waypoint(row, logger):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO waypoint("name", "description", "weather_condition", "weather_temperature", "uuid", "location_geom") 
+            VALUES(%s, %s, %s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s), %s));
+            """,
+            (row["display_name"], row["description"], row["weather_condition"], row["weather_temperature"], row["uuid"], row["location_lon"], row["location_lat"], 4326)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        logger.info(f"Add waypoint {row["display_name"]} to database")
+    except Exception as e:
+        logger.error(f"Fehler beim Einfügen des Waypoint: {e}")
+        logger.error(f"Fehler bei Zeile: {row}")  
+        raise e  
