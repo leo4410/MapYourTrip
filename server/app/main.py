44f16f7f905a2cfc5d796ddd23a58ae3426c1c38
Leonardo Seminatore <l02.seminatore@gmail.com>
2025-04-15 10:11:07 +0200

    add functionality for backend

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 from io import BytesIO
+import json
+import logging
 import os
+import pandas as pd
 import shutil
 import uuid
 import zipfile
 
 app = FastAPI()
+
+# add logger
+logging.basicConfig(level=logging.INFO)  
+logger = logging.getLogger(__name__)
 
 @app.post("/upload-zip/")
 async def upload_zip(file: UploadFile = File(...)):
@@ -21,9 +28,24 @@ async def upload_zip(file: UploadFile = File(...)):
             os.makedirs(extract_dir, exist_ok=True)
             # unzip file
             zip_ref.extractall(extract_dir)
-            # delete created directory
-            shutil.rmtree(extract_dir)
         
+        # open trip and extract data (need to us for loop later)
+        with open(f"{extract_dir}/trip/marokko_14732894/trip.json", "r", encoding="utf-8") as f:
+            daten = json.load(f)
+            steps = daten["all_steps"]
+
+        
+        # normalize json to extraxt nested fields
+        df = pd.json_normalize(steps, sep='_')  
+
+        # select relevant columns
+        auswahl = df[["id", "display_name", "description", "weather_condition", "weather_temperature", "uuid", "location_lon", "location_lat"]]
+        
+        logger.info(f"Ausgewählte Personen: {auswahl}")
+        
+        # delete created directory
+        shutil.rmtree(extract_dir)
+
         # good message   
         return {"message": f"ZIP-Datei erfolgreich entpackt nach {extract_dir}"}
 
