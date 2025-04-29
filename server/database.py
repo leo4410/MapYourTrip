d3eca83702375f88eb556dcee7cbe9d6bf2de65f
Marco Stampfli <marco.stampfli@students.fhnw.ch>
2025-04-29 09:39:10 +0200

    db conexion and config.json update

--- server/database.py
+++ server/database.py
@@ -1,12 +1,18 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import json
+
+def load_db_config():
+    with open('config.json', 'r') as f:
+        return json.load(f)
 
 def get_db_connection():
+    config = load_db_config()
     return psycopg2.connect(
-        dbname="db_test",
-        user="postgres",
-        password="postgres",
-        host="localhost",
-        port="5432",
+        dbname=config["dbname"],
+        user=config["user"],
+        password=config["password"],
+        host=config["host"],
+        port=config["port"],
         cursor_factory=RealDictCursor
     )
